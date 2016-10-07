e3357d39e377bf3937ee06e28190815a9e397bf4
NicholasArnaud <cobrahead09@gmail.com>
2016-10-07 14:03:01 -0500

    Finishes for Gist Assignments

---
+++ Make-Up Work/Cat.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+class Cat
+{
+	char m_name;
+
+public:
+	Cat() {};
+
+	void NameMe(char n)
+	{
+		m_name = n;
+	}
+};
--- Make-Up Work/Source.cpp
+++ Make-Up Work/Source.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include "Cat.h"
 
 float Stuff(float a, float b, float c);
 void TakeArray(int*a, int size);
-char Array(char back[]);
+void Array(char back[]);
+int Timez(int*t_array, int size);
 
 int main()
 {
 
-	
+
 	Stuff(10, 20, 30);
 	float Stuff(float a, float b, float c);
 
@@ -16,6 +18,13 @@ int main()
 
 	char array[] = "Hello";
 	Array(array);
+
+
+	//Cat mycats[25];
+
+
+	int entNum[8] = { 5,4,4,5,8,8,4,2 };
+	int t = Timez(entNum, 8);
 
 
 	printf("Nicholas Arnaud \nNA1163 \n");
@@ -44,15 +53,53 @@ void TakeArray(int*a, int size)
 }
 
 
-char Array(char back[])
+void Array(char back[])
 {
-	
-	for (int i = 4; i>0 ; i--)
+	int j = 0;
+	for (; back[j] != 0; j++);
+	int size = j;
+
+	for (int i = 0; i < j / 2; i++)
 	{
-		char temp = back[0];
-		back[0] = back[4];
-		back[4] = temp;
+		char temp = back[i];
+		back[i] = back[(size - 1) - i];
+		back[(size - 1) - i] = temp;
+	}
+}
+
+/*
+void nameCats(int it, Cat*c, int size)
+{
+	if (it < size)
+	{
+		c[it++].NameMe((it % 2 ==0 ) ? 65 + it : 97 + it);
+		nameCats(it, c, size);
+	}
+}
+*/
+
+
+int Timez(int*t_array, int size)
+{
+	int most;
+	int mostTimez = 0;
+	for (int i = 0; i < size; i++)
+	{
+		int timePlaced = 0;
+		for (int j=0; j < size; j++)
+		{
+			if (t_array[i] == t_array[j])
+			{
+				timePlaced++;
+				if (timePlaced > mostTimez)
+				{
+					most = t_array[j];
+					mostTimez = timePlaced;
+				}
+			}
+		}
+
 	}
 
-	return back[4];
+	return most;
 }
